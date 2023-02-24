621cba2181f6c3a362feeaa1fc6234f22055ca8a
Natalie Timms <nrtimms@Natalies-Air.attlocal.net>
2023-02-24 13:36:36 -0800

    added coin flipper images and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 // TODO: Declare any global variables we need
-
+let flipButton = document.querySelector("#flip")
+let clearButton = document.querySelector("#clear")
+let headsTable = document.querySelector("#heads")
+let headsPctTable = document.querySelector("#heads-percent")
+let tailsTable = document.querySelector("#tails")
+let tailsPctTable = document.querySelector("#tails-percent")
+let statusMessage = document.querySelector("#message")
+let image = document.querySelector("#penny-image")
+let headsPct =0
+let tailsPct =0
+let headsCount = 0
+let tailsCount = 0
+let total = 0
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
@@ -9,6 +21,27 @@ document.addEventListener('DOMContentLoaded', function () {
     // TODO: Add event listener and handler for flip and clear buttons
 
     // Flip Button Click Handler
+    flipButton.addEventListener("click", function(){
+        total ++
+        let result = Math.round(Math.random()*101)
+        console.log("result is ", result)
+        if (result<51){
+            image.src = "assets/images/penny-heads.jpg"
+            headsCount ++
+            headsTable.textContent = `${headsCount}`
+            statusMessage.textContent = "You Flipped Heads!"
+        }else{
+            image.src = "assets/images/penny-tails.jpg"
+            tailsCount ++
+            tailsTable.textContent = `${tailsCount}`
+            statusMessage.textContent = "You Flipped Tails!"
+        }
+        headsPct = Math.round(headsCount/total*100)
+        headsPctTable.textContent = `${headsPct}%`
+        tailsPct = Math.round(tailsCount/total*100)
+        tailsPctTable.textContent = `${tailsPct}%`
+
+    })
         // TODO: Determine flip outcome
         // TODO: Update image and status message in the DOM
 
@@ -21,6 +54,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     // Clear Button Click Handler
+    clearButton.addEventListener("click", function(){
+        statusMessage.textContent = "Let's Get Rolling!"
+        headsCount = 0
+        headsTable.textContent = `${headsCount}`
+        tailsCount = 0
+        tailsTable.textContent = `${tailsCount}`
+        headsPct = 0
+        headsPctTable.textContent = `${headsPct}%`
+        tailsPct = 0
+        tailsPctTable.textContent = `${tailsPct}%`
+    })
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
